72ee329b442ce8323c8b6b115bdc45ed1e38af9c
joegasewicz <joegasewicz@gmail.com>
2025-05-22 16:28:17 +0100

    v0.2.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='forestmq',
-    version='0.2.0',
+    version='0.2.1',
     description='Python client for ForestMQ',
     packages=["forestmq"],
     classifiers=[
